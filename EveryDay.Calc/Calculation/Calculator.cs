c3508fabcad8ca86256a3fd1f30a60ae3c96f3fa
Sieglien <sabotage_99@mail.ru>
2017-08-07 11:46:16 +0400

    Readme

---
+++ EveryDay.Calc/Calculation/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EveryDay.Calc.Calculation
+{
+    public class Calculator
+    {
+        public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Div(double x, double y)
+        {
+            return y == 0 ? 0 : x / y;
+        }
+
+        public double Sqrt(double x)
+        {
+            return Math.Sqrt(x);
+        }
+
+    }
+}
---
+++ EveryDay.Calc/EveryDay.Calc.Console/Program.cs
@@ -0,0 +1,53 @@
+﻿using EveryDay.Calc.Calculation;
+using SConsole = System.Console;
+
+namespace EveryDay.Calc.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oper = args[0].ToLower();
+            
+            var x = Str2Int(args[1]);
+
+            var y = Str2Int(args[2]);
+
+            var calc = new Calculator();
+
+            double result = 0;
+
+            if (oper == "sum")
+            {
+                result = calc.Sum(x, y);
+            }
+            else if (oper == "div")
+            {
+                result = calc.Div(x, y);
+            }
+            else if (oper == "sqrt")
+            {
+                result = calc.Sqrt(x);
+            }
+            else
+            {
+                SConsole.WriteLine("Нет такой операции");
+            }
+            
+            SConsole.WriteLine(result.ToString());
+
+            SConsole.ReadKey();
+        }
+
+
+        private static double Str2Int(string str)
+        {
+            double result;
+
+            if(!double.TryParse(str, out result)){
+                SConsole.WriteLine("Не удалось преобразовать \"{0}\" в число", str);
+            }
+            return result;
+        }
+    }
+}
